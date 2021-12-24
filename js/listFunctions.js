c093b8eb06e82dabe99f0168d3ce063cb74209bd
samdevbeltran <samigamedev96@gmail.com>
2021-12-24 12:27:24 -0500

    buscador hecho

--- js/listFunctions.js
+++ js/listFunctions.js
@@ -12,5 +12,41 @@ function showSlectedPhrase(id){
 	let result;
 	result = phrasesVar.filter(item => item["id"] == id);	
 	sessionStorage.setItem("presentPhrase", JSON.stringify(result));
-	window.location.href = "http://localhost/giftProyect/index.html";
+	window.location.href = "http://localhost/giftProyect/index.html#message-section";
 }
+
+
+function findByType(type,value){
+	value = String(value)
+	value = value.toLowerCase();
+	let result = [];	
+
+	phrasesVar.forEach(item => {
+		if(item[type].toLowerCase().indexOf(value) > -1){
+			result.push(item)  
+		}
+	});
+
+	return result;
+}
+
+function checkIfIdExists(array,list){
+	let numbersTrack = [];
+	if(array.length == 0){
+		return false;
+	}
+	if(array.length > 0){	
+		array.forEach(item => {
+			numbersTrack.push(item["id"])
+		})
+
+		if(!numbersTrack.includes(list["id"])){
+			array.push(list);
+		}
+	}
+}
+
+function reloadPage(){
+	location.reload();
+}
+
--- js/main.js
+++ js/main.js
@@ -92,13 +92,10 @@ function showPhraseList(){
 }
 
 function renderPhrasesList(list){
-	convertToShortPhrase(list).forEach(result =>{
-		
+	convertToShortPhrase(list).forEach(result =>{		
 		document.querySelector("#list-container > ul").innerHTML += '<li>'+(result["id"]+1)+'-<a onclick="showSlectedPhrase('+result["id"]+')">'+result["description"]+" "+ "..."+'</a></li><hr class="rounded-line">';
 		
 	});
-	
-	// convertToShortPhrase(list);
 }
 
 function convertToShortPhrase(list){
@@ -113,6 +110,59 @@ function convertToShortPhrase(list){
 	return convertList;	
 }
 
+if(document.getElementById("buscar-button2") != null){
+	let searchInput = document.getElementById("search-input");
+	document.getElementById("buscar-button2").addEventListener("click",function(){
+		let union = [];
+		
+		if(searchInput.value != ""){
+			
+			let byDescription = findByType("description",searchInput.value);
+			let byAuthor = findByType("author",searchInput.value);
+			if(byDescription.length > 0 && union == 0){
+				byDescription.forEach(item => {
+					union.push(item);
+				});
+				if(byAuthor.length > 0){
+					byAuthor.forEach(item =>{
+						checkIfIdExists(union,item);	
+					})
+					
+				}
+			}else if(byAuthor.length > 0 && union == 0){
+				byAuthor.forEach(item => {
+					union.push(item);
+				});
+				if(byDescription.length > 0){
+				 	byDescription.forEach(item=> {
+						checkIfIdExists(union,item);
+				 	});	
+				}
+			}
 
+			renderSearchedWord(union);
+		}
 
+	});
 
+	searchInput.addEventListener("keyup",(val)=>{
+		if(val.key == "Backspace"){
+			if(searchInput.value == ""){
+				document.querySelector("#list-container > ul").innerHTML = "";
+				showPhraseList();
+			}
+		}
+		
+	});
+}
+
+function renderSearchedWord(array){
+	let container = document.querySelector("#list-container > ul");
+	container.innerHTML = "";
+	if(array.length > 0){
+		renderPhrasesList(array);
+	}else{
+		container.innerHTML += '<li><a onclick="reloadPage()"><p style="text-align:center">No se encontraron coincidencias</p></a></li>';
+	}
+	
+}
